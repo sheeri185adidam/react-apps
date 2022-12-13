174688ac8932dc0bfd9ec3b6b3e1eb5cf6e3c834
Manik Sheeri <sheeri185adidam@gmail.com>
2022-12-13 16:37:28 -0700

    updated app

--- admin-dashboard/src/App.js
+++ admin-dashboard/src/App.js
@@ -1,8 +1,27 @@
-function App() {
-  return (
-    <div className="app">
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { ColorModeContext, useMode } from "./theme";
+import Topbar from "./scenes/global/topbar";
+import Dashboard from "./scenes/dashboard";
+import { Route, Routes } from "react-router-dom";
+import { Sidebar } from "react-pro-sidebar";
 
-    </div>
+function App() {
+  const [theme, colorMode] = useMode();
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="app">
+          <Sidebar />
+          <main className="content">
+            <Topbar />
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+            </Routes>
+          </main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
   );
 }
 
